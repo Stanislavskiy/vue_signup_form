8a9c8bac00b7adb629a44fbd066f6da1c2025857
Stanislavskiy <stanislavskiy.sanya@mail.ru>
2018-06-17 11:20:04 +0300

    Adding of an actions

--- src/store/modules/registration/actions.js
+++ src/store/modules/registration/actions.js
@@ -1,3 +1,30 @@
-const actions = {};
+const actions = {
+  clearRegistrationData({ commit }) {
+    /* 
+      Для очистки формы после отправки 
+      данных на сервер
+    */
+    commit("updateLogin", null);
+    commit("updatePassword", null);
+    commit("updateEmail", null);
+    commit("updateFirstName", null);
+    commit("updateLastName", null);
+    commit("updateCountry", null);
+    commit("updateCity", null);
+    commit("updateBirthDate", null);
+    commit("updateZipCode", null);
+  },
+
+  confirmRegistrationData({ commit }) {
+    /* 
+      Симулирует обработку данных регистрации
+      (отправка на сервер и т.д). Возвращает 
+      промис c задержкой
+    */
+    return new Promise((resolve, reject) => {
+      setTimeout(resolve, 1000);
+    });
+  }
+};
 
 export default actions;
